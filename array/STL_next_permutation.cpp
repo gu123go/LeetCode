52c818f7680135ff05604dcfa0aae91ae115f141
gu320go <1186529142@qq.com>
2018-03-03 14:41:02 +0800

    智障了

--- array/STL_next_permutation.cpp
+++ array/STL_next_permutation.cpp
@@ -10,41 +10,44 @@
 
 using namespace std;
 
-template<typename _BidirectionalIterator>
-inline bool 
-	next_permutation(_BidirectionalIterator __first, _BidirectionalIterator __last)
-{
-	return ::__next_permutation(__first, __last, __gnu_cxx::__ops::__iter_less_iter());
-}
-
-
-template<typename _BidirectionalIterator, typename _Compare>
-bool __next_permutation(_BidirectionalIterator __first, _BidirectionalIterator __last, _Compare __comp)
-{
-	if(__first == __last) return false;
-	_BidirectionalIterator __i = __first;
-	++__i;
-	if(__i == __last) return false;
-
-	__i = __last;
-	--__i;
-
-	for(; ;){
-		_BidirectionalIterator __ii = __i;
+namespace tt{
+	
+	template<typename _BidirectionalIterator, typename _Compare>
+	bool __next_permutation(_BidirectionalIterator __first, _BidirectionalIterator __last, _Compare __comp)
+	{
+		if(__first == __last) return false;
+		_BidirectionalIterator __i = __first;
+		++__i;
+		if(__i == __last) return false;
+	
+		__i = __last;
 		--__i;
-		if(__comp(__i, __ii)){
-			_BidirectionalIterator __j = __last;
-			while(!__comp(__i, --__j)){}
-			iter_swap(__i, __j);
-			reverse(__ii, __last);
-
-			return true;
-		}
-		if(__i == __last){
-			rever(__first, __last);
-			return false;
+	
+		for(; ;){
+			_BidirectionalIterator __ii = __i;
+			--__i;
+			if(__comp(__i, __ii)){
+				_BidirectionalIterator __j = __last;
+				while(!__comp(__i, --__j)){}
+				iter_swap(__i, __j);
+				reverse(__ii, __last);
+	
+				return true;
+			}
+			if(__i == __first){
+				std::reverse(__first, __last);
+				return false;
+			}
 		}
 	}
+	
+	template<typename _BidirectionalIterator>
+	inline bool 
+		next_permutation(_BidirectionalIterator __first, _BidirectionalIterator __last)
+	{
+		return tt::__next_permutation(__first, __last, __gnu_cxx::__ops::__iter_less_iter());
+	}
+	
 }
 
 
@@ -57,7 +60,7 @@ int main()
 		for(auto item : iv)
 			cout << item;
 		cout << endl;
-	}while(::next_permutation(iv.begin(), iv.end()));
+	}while(tt::next_permutation(iv.begin(), iv.end()));
 
 	return 0;
 }
